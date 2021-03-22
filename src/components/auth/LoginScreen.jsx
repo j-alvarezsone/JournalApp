7e85d7c460ec64f337deef951afa0a6bdc9ed479
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-03-22 07:38:17 +0100

    added redux

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { login } from '../../actions/auth';
+
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formValue, handleInputChange, reset] = useForm({
+    email: 'jorge@gmail.com',
+    password: '123456',
+  });
+
+  const { email, password } = formValue;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(1234, 'Jorge'));
+  };
+
   return (
     <>
       <h3 className='auth__title'>Login</h3>
-      <form action=''>
-        <input type='text' placeholder='Email' name='email' className='auth__input' autoComplete='off' />
-        <input type='password' placeholder='Password' name='password' className='auth__input' />
+      <form onSubmit={handleLogin}>
+        <input
+          type='text'
+          placeholder='Email'
+          name='email'
+          value={email}
+          className='auth__input'
+          autoComplete='off'
+          onChange={handleInputChange}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          value={password}
+          className='auth__input'
+          onChange={handleInputChange}
+        />
         <button className='btn btn-primary btm-block' type='submit'>
           Login
         </button>
